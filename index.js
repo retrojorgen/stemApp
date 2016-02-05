8c0f06b7a0adfb6826992135355a1690f315061f
Jørgen Jacobsen <retrojorgen@Jrgens-MacBook-Air.local>
2016-02-05 20:45:39 +0100

    updated

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.get('/test', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ a: 1 }));
 });
 
 app.listen(3000, function () {
